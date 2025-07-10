aeec04f0c8cca9f50892eddda89691466a50041a
Somalia0932 <42663232+Somalia0932@users.noreply.github.com>
2025-07-10 16:02:18 +0800

    Update schemas.py
    GeoInfo模型，增加城市和国家字段

--- app/schemas.py
+++ app/schemas.py
@@ -2,9 +2,11 @@ from pydantic import BaseModel
 from typing import Optional
 
 class GeoInfo(BaseModel):
-    latitude: float
-    longitude: float
-    time_zone: Optional[str]
+    city: Optional[str] = None
+    country: Optional[str] =  None
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+    time_zone: Optional[str] =  None
 
 class GeoIPResponse(BaseModel):
     api_version: str
